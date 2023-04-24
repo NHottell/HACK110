892d952fa1573c0e132cf329e4899cd2b5ddd534
Nick Hottell <nhottell@unc.edu>
2023-04-24 16:13:11 -0400

    Added base template and base homepage html.

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app: Flask = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/tracker')
+def habit_tracker():
+    return render_template('habit_tracker.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
